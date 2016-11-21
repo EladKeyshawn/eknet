9c5510cbdc00e5c97ee234d402d3389a20707246
Elad Keyshawn <elad.keyshawn@gmail.com>
2016-11-21 20:50:01 +0200

    updated main

--- main.py
+++ main.py
@@ -10,19 +10,20 @@ data = {}
 get_ip = 'ipconfig getifaddr en0'.split()
 get_submask = 'ipconfig getoption en0 subnet_mask'.split()
 get_dns = 'ipconfig getoption en0 domain_name_server'.split()
+get_default_gateway = 'netstat -nr | grep default'.split()
 flush_dns = 'dscacheutil -flushcache'.split()
 def get_stats():
     data['my_ip'] = subprocess.check_output(get_ip)
     data['netmask'] = subprocess.check_output(get_submask)
     data['dns'] = subprocess.check_output(get_dns)
-    # print data
+    data['gateway'] = subprocess.check_output(get_default_gateway).split()[11]
+# print data
 
 
 get_stats()
 eknet = Eknet(data)
 
-print eknet.get_ip_bin()
-print eknet.get_subnet_bin()
+
 
 eknet.display_raw_data()
 
